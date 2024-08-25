f48f838c5781229a38cd62d425fcb13c22c412d8
Stefka85 <stephan.kourilo@gmail.com>
2024-08-25 21:16:25 +0300

    Add files via upload

---
+++ module 3.1..py
@@ -0,0 +1,28 @@
+test = ('(8, CAPYBARA, capybara), '
+        '(10, ARMAGEDDON, armageddon), '
+        'True, '
+        'False, '
+        '4')
+print(test)
+
+
+calls = 0
+def count_calls ():
+    global calls
+    calls += 1
+
+def string_info (string):
+    count_calls()
+    return (len(string), string.upper(), string.lower())
+
+def is_contains(string, list_to_search):
+    count_calls()
+    return string.lower() in map(str.lower, list_to_search)
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic']))  # No matches
+print(calls)
+
